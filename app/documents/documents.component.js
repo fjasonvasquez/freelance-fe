05407041b705feee73caa98e32a3ce9395e891d7
fjasonvasquez <fjvasquez@aol.com>
2016-12-20 23:33:40 -0800

    created a document management component, links between pages, interface with model data in Angular 2

--- app/documents/documents.component.js
+++ app/documents/documents.component.js
@@ -11,6 +11,30 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require("@angular/core");
 var DocumentsComponent = (function () {
     function DocumentsComponent() {
+        this.pageTitle = "Document Dashboard";
+        this.documents = [
+            {
+                title: "My First Doc",
+                description: 'asdfasdf asdfasd',
+                file_url: 'http://google.com',
+                updated_at: '11/11/16',
+                image_url: 'http://google.com',
+            },
+            {
+                title: "My Second Doc",
+                description: 'asdfasdf asdfasd',
+                file_url: 'http://google.com',
+                updated_at: '11/11/16',
+                image_url: 'http://google.com',
+            },
+            {
+                title: "My Last Doc",
+                description: 'asdfasdf asdfasd',
+                file_url: 'http://google.com',
+                updated_at: '11/11/16',
+                image_url: 'http://google.com',
+            }
+        ];
     }
     return DocumentsComponent;
 }());
