b2450f807dc96a00aa54fd717e34696f37256642
fjasonvasquez <fjvasquez@aol.com>
2016-12-22 22:46:26 -0800

    Angular List View with Data

--- app/proposal/proposal-list.component.js
+++ app/proposal/proposal-list.component.js
@@ -9,8 +9,17 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
+var proposal_1 = require("./proposal");
 var ProposalListComponent = (function () {
     function ProposalListComponent() {
+        this.proposalOne = new proposal_1.Proposal(15, 'Abc Company', 'http://portfolio.jasonvasquez.com', 'Ruby on Rails', 150, 120, 15, 'fjvasquez78@gmail.com');
+        this.proposalTwo = new proposal_1.Proposal(99, 'Xyz Company', 'http://portfolio.jasonvasquez.com', 'Ruby on Rails', 150, 120, 15, 'fjvasquez78@gmail.com');
+        this.proposalThree = new proposal_1.Proposal(300, 'Something Company', 'http://portfolio.jasonvasquez.com', 'Ruby on Rails', 150, 120, 15, 'fjvasquez78@gmail.com');
+        this.proposals = [
+            this.proposalOne,
+            this.proposalTwo,
+            this.proposalThree
+        ];
     }
     return ProposalListComponent;
 }());
